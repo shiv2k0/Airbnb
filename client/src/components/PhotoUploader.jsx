9835bdb5cec3275efc63594755c39a7282509849
Shivendra Singh <shivendrasingh@Shivendras-Mac-mini.local>
2023-06-22 09:54:29 +0530

    Deleting Photos and selecting photos as main photos

--- client/src/components/PhotoUploader.jsx
+++ client/src/components/PhotoUploader.jsx
@@ -1,4 +1,5 @@
 import { BiUpload } from "react-icons/bi";
+import { AiTwotoneDelete, AiFillStar, AiOutlineStar } from "react-icons/ai";
 import axios from "axios";
 import { useState } from "react";
 
@@ -34,6 +35,16 @@ const PhotoUploader = ({ addedPhotos, onChange }) => {
       });
   };
 
+  const deletePhoto = (filename) => {
+    onChange([...addedPhotos.filter((photo) => photo !== filename)]);
+  };
+  const mainPhoto = (filename) => {
+    const addedPhotosWithoutSelected = addedPhotos.filter(
+      (photo) => photo !== filename
+    );
+    onChange([filename, ...addedPhotosWithoutSelected]);
+  };
+
   return (
     <>
       <div className="flex gap-4  ">
@@ -53,12 +64,30 @@ const PhotoUploader = ({ addedPhotos, onChange }) => {
       <div className=" mt-2 grid gap-2 grid-cols-3 lg:grid-cols-6 md:grid-cols-4  ">
         {addedPhotos.length > 0 &&
           addedPhotos.map((link) => (
-            <div className="h-32 flex" key={link}>
+            <div className="h-32 flex relative" key={link}>
               <img
                 className="rounded-2xl w-full object-cover "
                 src={`http://localhost:8080/uploads/${link}`}
                 alt=""
               />
+              <div
+                onClick={() => deletePhoto(link)}
+                className="absolute right-2 bottom-2 h-6 w-6 bg-white flex justify-center items-center rounded-full cursor-pointer"
+              >
+                <AiTwotoneDelete size={12} color="f53850" />
+              </div>
+              {link === addedPhotos[0] ? (
+                <div className="absolute left-2 top-2 h-6 w-6 bg-white flex justify-center items-center rounded-full cursor-pointer">
+                  <AiFillStar size={12} color="f53850" />
+                </div>
+              ) : (
+                <div
+                  onClick={() => mainPhoto(link)}
+                  className="absolute left-2 top-2 h-6 w-6 bg-white opacity-80 flex justify-center items-center rounded-full cursor-pointer"
+                >
+                  <AiOutlineStar size={12} color="f53850" />
+                </div>
+              )}
             </div>
           ))}
         <label className="cursor-pointer border bg-transparent rounded-2xl p-8 text-2xl flex text-gray-600 gap-2 items-center justify-center ">
